51821167243a35251defcd4b6409ca4401057584
hrakaralkhidher <114784184+hrakaralkhidher@users.noreply.github.com>
2022-12-07 18:15:42 -0600

    Initial commit

---
+++ src/test/java/firstcode/FirstSeleniumCode.java
@@ -0,0 +1,23 @@
+package firstcode;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumCode {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //set Up chrome driver
+        WebDriverManager.chromedriver().setup();
+
+        //create an instance of the driver
+
+        WebDriver driver = new ChromeDriver();
+
+        //navigate to chrome browser
+        driver.get("https://www.google.com/");
+        
+
+
+    }
+}
