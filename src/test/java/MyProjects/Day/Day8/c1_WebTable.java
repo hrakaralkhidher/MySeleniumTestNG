89be82311d562a13c0e8b1d72fbf7f449937915b
hrakaralkhidher <114784184+hrakaralkhidher@users.noreply.github.com>
2022-12-27 21:03:53 -0600

    first commit

---
+++ src/test/java/MyProjects/Day/Day8/c1_WebTable.java
@@ -0,0 +1,44 @@
+package MyProjects.Day.Day8;
+
+import MyProjects.Day.utilities.BrowserUtils;
+import MyProjects.Day.utilities.SmartBearUtils;
+import MyProjects.Day.utilities.WebDriverUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class c1_WebTable {
+//    Smartbear software street verification
+//       1. Open browser and login to:http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx
+//       2. Click on View all orders
+//       3. Verify Mark Smith has street as  "9, Maple Valley"
+    WebDriver driver;
+    @BeforeMethod
+    public void SetUp(){
+        driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        SmartBearUtils.LoginForSmartBearUtil(driver);
+    }
+        @AfterMethod
+    public void closing(){
+        BrowserUtils.Wait(4);
+        //driver.close();
+    }
+    @Test
+    public void TC1_StreetVerfication(){
+        WebElement markStreet = driver.findElement(By.xpath("//table[@class='SampleTable']/tbody/tr[3]/td[6]"));
+        System.out.println("mark street name: "+markStreet.getText());
+        String actual = markStreet.getText();
+        String expected = "9, Maple Valley";
+        Assert.assertEquals(actual,expected,"StreetName Verification has failed!!!");
+
+    }
+}
--- src/test/java/MyProjects/Day/utilities/SmartBearUtils.java
+++ src/test/java/MyProjects/Day/utilities/SmartBearUtils.java
@@ -3,6 +3,9 @@ package MyProjects.Day.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
 
 public class SmartBearUtils {
     public static void LoginForSmartBearUtil(WebDriver driver){
@@ -12,6 +15,28 @@ public class SmartBearUtils {
         password.sendKeys("test");
         WebElement LoginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
         LoginButton.click();
+    }
+    public static void negativeLoginForSmartBearUtil(WebDriver driver, String userID, String passwordID){
+        WebElement userName = driver.findElement(By.id("ctl00_MainContent_username"));
+        userName.sendKeys(userID);
+        WebElement password = driver.findElement(By.id("ctl00_MainContent_password"));
+        password.sendKeys(passwordID);
+        WebElement LoginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
+        LoginButton.click();
+    }
+
+    public static void VerifyStreet(WebDriver driver, String StreetName){
+        //to find the table // //table[@id='ctl00_MainContent_orderGrid']
+        //to find all table row //table[@id='ctl00_MainContent_orderGrid']//tr
+
+        List<WebElement> StreetsList=driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr//td[6]"));
+                for(WebElement eachStreet: StreetsList){
+                    if(eachStreet.getText().equals(StreetName)){
+                        Assert.assertTrue(eachStreet.getText().equals(StreetName));
+                        return;
+                    }
+                }
+                Assert.fail("Street name: "+StreetName+"is not present on the table");
     }
 
 
