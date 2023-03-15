c2881a8dc7a0aee4c3b3049c143f30f2713c1736
hrakaralkhidher <114784184+hrakaralkhidher@users.noreply.github.com>
2023-03-15 13:48:07 -0500

    first commit

--- src/test/java/MyProjects/StudyHall/Day04_MonkeyTesting.java
+++ src/test/java/MyProjects/StudyHall/Day04_MonkeyTesting.java
@@ -12,51 +12,41 @@ import java.util.List;
 
 public class Day04_MonkeyTesting {
 
-    //Monkey Testing you as on user click the functionality randomly
-    // you do not follow the steps you try to break the app by clicking random links.
-    //"ul[data-ui='top-nav-category-list'] li a" ====> this is how to get the random link all together by the css selector
+    //Monkey Testing:You as an user click the functionalities randomly
+    //you do not follow the steps. You try to break the app by clicking random links.
+        //ul[data-ui='top-nav-category-list'] li a
 
-
-
-    @Test
-    public void monkeyTesting(){
+@Test
+public void monkeyTesting(){
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.etsy.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        List<WebElement> linklist= driver.findElements(By.cssSelector("ul[data-ui='top-nav-category-list'] li a"));
+      List<WebElement> linkList = driver.findElements(By.cssSelector("ul[data-ui='top-nav-category-list'] li a"));
 
-        int headerCount = linklist.size();
+      int headerCount = linkList.size();
         System.out.println(headerCount);
 
         System.out.println();
 
-        for (int i = 0; i < headerCount; i++) {
-            // this will basically choose randomly by the index if you dont put this then the forloop will go in order
-            // of the index/ so this is choosing the links randomly
-            int randomIndex = (int) Math.floor(Math.random() * headerCount);// randomly clicking  <*>
-            System.out.println(randomIndex);
+        for (int i=0 ; i<headerCount; i++ ){
+                int randomIndex = (int) Math.floor(Math.random() * headerCount);
+                System.out.println(randomIndex);
 
-            WebElement element = linklist.get(randomIndex);
-            System.out.println(element.getText());
-            element.click();
-            driver.navigate().back();
-//            element.click();
-            // if we use element.click() again, selenium will not recognize and you'll get staleElementReferenceException
-            //the Reason for that you can not use sanme element when you navigate back because it counted as autdated
-            //you need to declare that element again to avoid exception
-            linklist = driver.findElements(By.cssSelector("ul[data-ui='top-nav-category-list'] li a"));
-
-
-
-
+                WebElement element = linkList.get(randomIndex);
+                System.out.println(element.getText());
+                element.click();
+                driver.navigate().back();
+                //element.click()
+            /*If we use element.click() again, Selenium will not recognise and you'll get StaleElementReferenceException
+            The reason for that you can not use same element when you navigate back Because it's counted as outdated.
+            You need to declare that element again to avoid exception
+            * */
+                 linkList=driver.findElements(By.cssSelector("ul[data-ui='top-nav-category-list'] li a"));
         }
-
-
-    }
-
+}
 
 
 
